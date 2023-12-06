ae193c5246fdfbf77646aeb2aaf6af0ccc07732d
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2023-12-06 15:44:11 +0100

    Added all files

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/*****
+    SNACK 1
+*****/
+
+// Creo un array di 5 bici con proprietà: nome e peso
+let bikes = [
+    {
+        nome : 'BMC',
+        peso : 110
+    },
+    {
+        nome : 'Gazelle',
+        peso : 95
+    },
+    {
+        nome : 'Cipollini',
+        peso : 100
+    },
+    {
+        nome : 'Trek',
+        peso : 90
+    },
+    {
+        nome : 'Bergamont',
+        peso : 120
+    }
+]
